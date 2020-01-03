1b3ecfc8da5c1e402136580a9f0dff5c40045a5c
AbdulRazaq Suleiman <razaqayomide01@gmail.com>
2020-01-03 13:24:39 +0100

    Finished styling input component

--- src/components/Search/StyledSearch.js
+++ src/components/Search/StyledSearch.js
@@ -1,18 +1,40 @@
 import styled from 'styled-components';
 
-
 const StyledForm = styled.form`
-
+  max-width: 40rem;
+  margin: auto;
+  margin-top: 6rem;
 `;
 
 export const StyledLabel = styled.label`
-`
+  display: block;
+  text-align: center;
+  margin-bottom: 1.5rem;
+  text-transform: uppercase;
+  font-weight: 400;
+  font-size: 1.3em;
+  transition: all .8s ease-out;
 
-export const StyledInput = styled.div`
-  
+  &:hover {
+    transform: rotateY(180deg);
+  }
 `;
 
 export const StyledSearchInput = styled.input`
+  width: 100%;
+  padding: 1rem 1rem;
+  border: none;
+  border: 1px solid #ddd;
+  outline: none;
+  font-size: inherit;
+  font-weight: 400;
+  border-radius: 5px;
+  box-shadow: 0 2px 3px #999;
+  transition: all 0.3s ease-out;
+
+  &:focus {
+    border: 1px solid #aaa;
+  }
 `;
 
 export default StyledForm;
--- src/components/Search/index.js
+++ src/components/Search/index.js
@@ -1,19 +1,16 @@
 import React from 'react';
-import { faSearch } from "@fortawesome/free-solid-svg-icons";
+import { faSearchLocation } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
-import StyledForm, { StyledLabel, StyledInput, StyledSearchInput } from './StyledSearch';
+import StyledForm, { StyledLabel, StyledSearchInput } from './StyledSearch';
 
 const Search = () => {
   return (
     <StyledForm>
-      <StyledLabel htmlFor="search">Search Country</StyledLabel>
-      <StyledInput>
-        <FontAwesomeIcon icon={faSearch}/>
-        <StyledSearchInput type="search" name="search" aria-label="Search" id="search"/>
-      </StyledInput>
+      <StyledLabel htmlFor="search"><FontAwesomeIcon icon={faSearchLocation}/> Search Country</StyledLabel>
+        <StyledSearchInput type="search" name="search" aria-label="Search" id="search" placeholder="Search country by name, capital..."/>
     </StyledForm>
   );
 }
-
+ 
 export default Search;
