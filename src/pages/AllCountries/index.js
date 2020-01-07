769ca2b23dfbadbaafaf8c7bb704d5cd2416de61
AbdulRazaq Suleiman <razaqayomide01@gmail.com>
2020-01-07 23:07:19 +0100

    Added filter continent feature to AllCountries component

--- src/pages/AllCountries/index.js
+++ src/pages/AllCountries/index.js
@@ -1,13 +1,20 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { fetchAllCountries } from '../../store/actions';
 import { useDispatch, useSelector } from 'react-redux';
+import styled from 'styled-components';
 
 import Spinner from '../../components/Spinner';
 import Header from '../../components/Header';
 import AllCountriesCard from './AllCountriesCard';
 
 
+const StyledSelect = styled.select`
+  
+`;
+
+
 const AllCountries = () => {
+  const [continent, setContinent] = useState(null);
   const dispatch = useDispatch();
   const allCountries = useSelector(state => state.allCountries);
 
@@ -15,14 +22,27 @@ const AllCountries = () => {
     dispatch(fetchAllCountries());
   }, []);
 
-  const continentSelect = () => {
-    
+  const handleSelect = event => {
+    setContinent(event.target.value);
+  }
+
+  const ContinentSelect = () => {
+    return (
+      <StyledSelect name="continents" id="continent" onChange={handleSelect}>
+        <option value="africa">Africa</option>
+        <option value="asia">Asia</option>
+        <option value="americas">Americas</option>
+        <option value="oceania">Oceania</option>
+        <option value="australia">Australia</option>
+      </StyledSelect>
+    )
   }
 
   if (allCountries.length !== 0) {
     return (
       <>
       <Header />
+      <ContinentSelect />
       <AllCountriesCard />
       </>
     );
