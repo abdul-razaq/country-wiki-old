f2cd8e5ee78932f562586252fe002f7982c4b356
AbdulRazaq Suleiman <razaqayomide01@gmail.com>
2020-01-10 18:12:03 +0100

    modified App

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import { createStore, applyMiddleware, compose } from 'redux';
 import { Provider } from 'react-redux';
 import thunk from 'redux-thunk';
 
-import App from './components/App';
+import App from './components/App/index';
 import rootReducer from './store/reducers';
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
